aa881c46c3a117b12a21ba289be83527321cb8bb
markgmz92 <markgmz92@gmail.com>
2022-09-07 19:05:05 +0800

    [SW - 006] Added page query variables

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from 'react-query';
 import Planet from './Planet';
 
-const fetchPlanets = async () => {
-  const res = await fetch('http://swapi.dev/api/planets/');
+const fetchPlanets = async (key, greeting, page) => {
+  console.log(greeting);
+  const res = await fetch(`http://swapi.dev/api/planets/?page=${page}`);
   return res.json();
 };
 
 const Planets = () => {
-  const { data, status } = useQuery('planets', fetchPlanets, {
-    staleTime: 0,
-    //cacheTime: 10000,
-    onSuccess: () => console.log('Data fetched with no problems')
-  });
+  const [page, setPage] = useState(1);
+  const { data, status } = useQuery(
+    ['planets', 'hello, ninjas', page],
+    fetchPlanets
+  );
   console.log(data);
 
   return (
     <div>
       <h2>Planets</h2>
+
+      <button onClick={() => setPage(1)}>page 1</button>
+      <button onClick={() => setPage(2)}>page 2</button>
+      <button onClick={() => setPage(3)}>page 3</button>
       {/* <p>{status}</p> */}
       {status === 'loading' && <div>Loading data...</div>}
       {status === 'error' && <div>Error fetching data</div>}
